f60e715046fdde99f7f699211abc88051d01f28c
Olga Kovalyshyn <seren_d1_pity@hotmail.com>
2016-12-06 20:49:04 +0200

    in progress

--- selenium_project/src/test/java/sel/course/WindowsTest.java
+++ selenium_project/src/test/java/sel/course/WindowsTest.java
@@ -19,14 +19,24 @@ public class WindowsTest extends SetDriver {
         wd.navigate().to("http://localhost/litecart/admin/?app=countries&doc=countries");
         wd.findElement(By.xpath("//table[contains(@class, 'dataTable')]//tr[2]/td[7]/a")).click();
 
-        String mainWindow = wd.getWindowHandle();
-        System.out.println(mainWindow);
-        //Set<String> oldWindows = wd.getWindowHandles();
-        //System.out.println("oldWindows");
-        WebElement openLink = wd.findElement(By.xpath(".//*[@id='content']/form/table[1]/tbody/tr[2]/td/a/i"));
-        ((JavascriptExecutor) wd).executeScript("window.open(\"http://www.google.com/\");");
+        String windowBefore = wd.getWindowHandle();
+        System.out.println(windowBefore);
 
+        WebElement link = wd.findElement(By.xpath(".//*[@id='content']/form/table[1]/tbody/tr[2]/td/a"));
+        String linkUrl = link.getAttribute("href");
+        System.out.println(linkUrl);
+        //((JavascriptExecutor) wd).executeScript("window.open(\"http://www.google.com/\");");
+        ((JavascriptExecutor) wd).executeScript("window.open("+linkUrl+");");
 
+        for(String winHandle : wd.getWindowHandles()){
+            wd.switchTo().window(winHandle);
+            String windowNew = wd.getWindowHandle();
+            System.out.println(windowNew);
+        }
+
+        wd.findElement(By.cssSelector("div.jsb input[name='btnI']")).click();
+        wd.close();
+        wd.switchTo().window(windowBefore);
 
 
     }
